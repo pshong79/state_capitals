1e4cfcfbc652ede68657efe066cec4d6d28251a2
-- <-->
2023-12-03 19:35:08 -0600

    Initial commit containg .gitignore, README.md, and working program.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Description
+I created this as a tool for our daughter to learn her state capitals.
+
+## To run
+To run this program, `cd` into the directory and run:
+```
+$ ruby state_capitals.rb
+```
---
+++ state_captials.rb
@@ -0,0 +1,72 @@
+states_and_capitals = {
+  alabama: "montgomery",
+  alaska: "juneau",
+  arizona: "phoenix",
+  arkansas: "little rock",
+  california: "sacramento",
+  colorado: "denver",
+  connecticut: "hartford",
+  delaware: "dover",
+  florida: "tallahassee",
+  georgia: "atlanta",
+  hawaii: "honolulu",
+  idaho: "boise",
+  illinois: "springfield",
+  indiana: "indianapolis",
+  iowa: "des moines",
+  kansas: "topeka",
+  kentucky: "frankfort",
+  louisiana: "baton rouge",
+  maine: "augusta",
+  maryland: "annapolis",
+  massachusetts: "boston",
+  michigan: "lansing",
+  minnesota: "st. paul",
+  mississippi: "jackson",
+  missouri: "jefferson city",
+  montana: "helena",
+  nebraska: "lincoln",
+  nevada: "carson city",
+  new_hampshire: "concord",
+  new_jersey: "trenton",
+  new_mexico: "santa fe",
+  new_york: "albany",
+  north_carolina: "raleigh",
+  north_dakota: "bismark",
+  ohio: "columbus",
+  oklahoma: "oklahoma city",
+  oregon: "salem",
+  pennsylvania: "harrisburg",
+  rhode_island: "providence",
+  south_carolina: "columbia",
+  south_dakota: "pierre",
+  tennessee: "nashville",
+  texas: "austin",
+  utah: "salt lake city",
+  vermont: "montpelier",
+  virginia: "richmond",
+  washington: "olympia",
+  west_virginia: "charleston",
+  wisconsin: "madison",
+  wyoming: "cheyenne"
+}
+
+answered = {}
+
+while states_and_capitals.size > 0 do
+  state_keys = states_and_capitals.keys
+  state = state_keys[rand(state_keys.size)]
+
+  print "What is the capital of #{state}? "
+  user_capital = gets.chomp
+
+  if user_capital.downcase == states_and_capitals[state]
+    answered[state] = states_and_capitals[state]
+    states_and_capitals.delete(state)
+    states_and_capitals.size
+    puts "Correct! #{user_capital.downcase}, #{state}. #{answered.size}/50 done."
+  else
+    puts "Incorrect. Try again."
+  end
+end
+puts "All done. Good job!"
