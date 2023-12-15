dc590282aafdb7e2c063eee992ba6d747e75e3e9
-- <-->
2023-12-14 21:24:30 -0600

    Version 2.1 - Removes capital from list if answered correctly

--- state_captials.rb
+++ state_captials.rb
@@ -51,7 +51,7 @@ states_and_capitals = {
   wyoming: "cheyenne"
 }
 
-CAPITALS = [ "albany", "annapolis", "atlanta", "augusta", "austin", "baton rouge", "bismark", "boise", "boston", "carson city", 
+capitals = [ "albany", "annapolis", "atlanta", "augusta", "austin", "baton rouge", "bismark", "boise", "boston", "carson city", 
              "charleston", "cheyenne", "columbia", "columbus", "concord", "denver", "des moines", "dover", "frankfort", "harrisburg",
              "hartford", "helena", "honolulu", "indianapolis", "jackson", "jefferson city", "juneau", "lansing", "lincoln", "little rock", 
              "madison", "montgomery", "montpelier", "nashville", "oklahoma city", "olympia", "phoenix", "pierre", "providence", "raleigh",
@@ -69,12 +69,13 @@ while states_and_capitals.size > 0 do
   state_keys = states_and_capitals.keys
   state = state_keys[rand(state_keys.size)]
   
-  puts "Here is a list of all the state capitals:\n#{CAPITALS}.\n\n"
+  puts "Here is a list of all the state capitals:\n#{capitals}.\n\n"
   print "What is the capital of #{state}? "
   user_capital = gets.chomp
 
   if user_capital.downcase == states_and_capitals[state]
     correct_answers[state] = states_and_capitals[state]
+
     puts "Correct! #{user_capital.downcase}, #{state}. #{correct_answers.size}/50 correct.\n\n"
   else
     wrong_answers[state] = states_and_capitals[state]
@@ -82,6 +83,7 @@ while states_and_capitals.size > 0 do
   end
 
   states_and_capitals.delete(state)
+  capitals.delete(user_capital)
   states_and_capitals.size
 end
 puts "All done. You got #{correct_answers.size}/50 correct.\n\n"
